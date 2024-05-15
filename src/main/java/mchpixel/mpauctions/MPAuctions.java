10b2bad220bf28d5f452092a36b69c00a890d2b1
MCHPixel <schulzefabian@outlook.de>
2024-05-15 22:29:26 +0200

    NIce

---
+++ src/main/java/mchpixel/mpauctions/MPAuctions.java
@@ -0,0 +1,17 @@
+package mchpixel.mpauctions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MPAuctions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
